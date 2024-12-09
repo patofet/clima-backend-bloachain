d675517358b12267917257d51e01c5e4dfcdf344
David Bruguera <david@jobchain.com>
2024-12-09 10:56:46 +0100

    dependencies

--- project/hardhat.config.ts
+++ project/hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition";
 
 const config: HardhatUserConfig = {
   networks: {
