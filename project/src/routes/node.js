3d842e75f9ac0cab6cc69b347d7ad6f467301820
David Bruguera <david@jobchain.com>
2025-10-08 13:25:24 +0200

    Add timestamp difference calculation between API and node in node status

--- project/src/routes/node.js
+++ project/src/routes/node.js
@@ -9,8 +9,11 @@ const nodes = [
 async function getNodeStatus(node) {
   try {
     const web3 = new Web3(new Web3.providers.HttpProvider(node.url, { timeout: 5000 }));
-    const [blockNumber, isSyncing, peerCount, networkId, gasPrice, hashrate, coinbase, clientVersion] = await Promise.all([web3.eth.getBlockNumber(), web3.eth.isSyncing(), web3.eth.net.getPeerCount(), web3.eth.net.getId(), web3.eth.getGasPrice(), web3.eth.getHashrate(), web3.eth.getCoinbase().catch(() => "No disponible"), web3.eth.getNodeInfo().catch(() => "No disponible")]);
-
+    const [blockNumber, isSyncing, peerCount, networkId, gasPrice, hashrate, coinbase, clientVersion, timestampNode] = await Promise.all([web3.eth.getBlockNumber(), web3.eth.isSyncing(), web3.eth.net.getPeerCount(), web3.eth.net.getId(), web3.eth.getGasPrice(), web3.eth.getHashrate(), web3.eth.getCoinbase().catch(() => "No disponible"), web3.eth.getNodeInfo().catch(() => "No disponible"), web3.eth.getBlock("latest").then((block) => block.timestamp)]);
+    const response = await fetch(`http://magiinterface.udg.edu:3000/login?address=0xbb678ed4adb678bad4b8f7203135ae1854463a7f&message=44`);
+    const data = await response.json();
+    const timestampAPI = data.timestamp;
+    const diferenceTimestamp = Math.abs(timestampAPI - timestampNode);
     return {
       name: node.name,
       url: node.url,
@@ -23,6 +26,7 @@ async function getNodeStatus(node) {
       hashrate: `${(Number(hashrate) / 1000000).toFixed(2)} MH/s`,
       coinbase: coinbase,
       clientVersion: clientVersion,
+      diferenceTimestamp: diferenceTimestamp.toString(),
     };
   } catch (error) {
     return {
